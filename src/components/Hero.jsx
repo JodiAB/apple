f685fed69283268907c4929a893f7d6fc5a0e6a2
unknown <jodiabrahams9@gmail.com>
2024-05-09 16:21:13 +0200

    crying

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='w-full nav-height bg-black relative'>
+    <div className='h-5/6'>
+    
+    </div>
+      </section>
+  )
+}
+
+export default Hero
